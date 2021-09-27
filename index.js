ffbdec80b36d68f178ba19c40c928e4640a14185
ciockie <ciockie.21@gmail.com>
2021-09-27 18:21:07 +0700

    add server

--- index.js
+++ index.js
@@ -36,33 +36,48 @@ async function remove_roles(member) {
     MuteUser.roles.remove(MuteUser.roles.cache)
 }
 
-
+//0,30
 var job = new CronJob('0 0,30 * * * *', async function() {
 
 	let a = await getnick();
         //console.log(a)
         let str = [];
+        let remove_list = [];
         if(a != false) {
             a.members.cache.forEach(member => {
                 if(check_role(member)) {
-                    if(member.displayName.includes('Aquila') || member.displayName.includes('aquila')) {
+                    if(
+                        member.displayName.toLowerCase().includes('aquila') || 
+                        member.displayName.toLowerCase().includes('bootes') ||
+                        member.displayName.toLowerCase().includes('cassiopeia') ||
+                        member.displayName.toLowerCase().includes('draco')
+                        ) {
                         //console.log(`${member.displayName} : ${member}`)
-                        let argsn = member.displayName.split(/ +/);
-                        //console.log(args)
-                        if(argsn.length == 2) {
-                            if(argsn[1].toLowerCase().includes('aquila') && argsn[1].endsWith(')')) {
-                                str.push(`${member.displayName} : ${member}\n`)
-								console.log(`${member.displayName} : ${member}\n`)
-                                set_roles(member)
-                            }
-                        } else if(argsn.length == 3) {
-                            if (argsn[2].toLowerCase().includes('aquila') && argsn[2].endsWith(')')) {
+                        let argsn = member.displayName.split(/ +/); 
+                        if(member.displayName.toLowerCase().endsWith(')')) {
+                            if(member.displayName.toLowerCase().split(" ").length - 1 < 3 && member.displayName.toLowerCase().split(" ").length - 1 > 0) {
                                 str.push(`${member.displayName} : ${member}\n`)
                                 set_roles(member)
-								console.log(`${member.displayName} : ${member}\n`)
+                                console.log(`Add : ${member.displayName} : ${member}`)
                             }
                         }
                     }
+                } else if(member.roles.cache.has('778532539287207956')) {
+                    if(member.roles.cache.has('778856916927250443')) return //MM Roles
+                    if(
+                    member.displayName.toLowerCase().includes('aquila') || 
+                    member.displayName.toLowerCase().includes('bootes') ||
+                    member.displayName.toLowerCase().includes('cassiopeia') ||
+                    member.displayName.toLowerCase().includes('draco')
+                    ) {
+                        //console.log(`${member.displayName} : ${member}`)
+                    } else {
+                        /*************** REMOVE FUNCTION *******************/
+                        //remove_list.push(`${member.displayName} : ${member}`)
+                        //console.log(`Remove : ${member.displayName} : ${member}`)
+                        //remove_roles(member)
+                    }
+                    //console.log(`${member.displayName} : ${member}`)
                 }
             });
             } else {
@@ -70,6 +85,7 @@ var job = new CronJob('0 0,30 * * * *', async function() {
                 channel_.send(`ERR`)
             }
             if(str.length > 0) {
+                if(prefix == '$') return;
                 channel_ = client.channels.cache.find(x => x.id == log_chnn);
                 channel_.send(`**Added Role** : \`\`\`${str.join('')}\`\`\``)
             }
